01f4a938ceda9523914964aa1ac141187564bd11
Miguel Baniqued <bmiguelcedric@gmail.com>
2024-06-01 13:21:17 -0700

    Color constants

---
+++ frontend/src/constants/colors.js
@@ -0,0 +1,13 @@
+// NOTE: If you want to use this file, you must also update the index.js file to use the COLORS object.
+
+export const COLORS = {
+    PRIMARY: '#FFC72C',
+    PRIMARY_DARK: '#F8B600',
+    /* SECONDARY: TBD */
+    WHITE: '#FFFAFA',
+    WHITE_DARK: '#EFEDEC',
+    BLACK: '#333333',
+    CTA_BUTTON: '#D42E2E',
+    CTA_BUTTON_DARK: '#AC2323',
+    LINK: '#38B6FF',
+};
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
+import { COLORS } from './constants/colors';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+
+const rootElement = document.documentElement;
+rootElement.style.setProperty('--color-primary', COLORS.PRIMARY);
+rootElement.style.setProperty('--color-primary-dark', COLORS.PRIMARY_DARK);
+rootElement.style.setProperty('--color-white', COLORS.WHITE);
+rootElement.style.setProperty('--color-white-dark', COLORS.WHITE_DARK);
+rootElement.style.setProperty('--color-black', COLORS.BLACK);
+rootElement.style.setProperty('--color-cta-button', COLORS.CTA_BUTTON);
+rootElement.style.setProperty('--color-cta-button-dark', COLORS.CTA_BUTTON_DARK);
+rootElement.style.setProperty('--color-link', COLORS.LINK);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +25,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
